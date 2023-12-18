f58b3cb0fc6fd0fc3b459ce5fe8a7d4e8d34289f
Mircea Marinescu <36738860+Mircea2004@users.noreply.github.com>
2023-12-18 16:21:41 -0700

    doc 0.1.0
    update readthedocs requirements

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2023, Mircea Marinescu'
 author = 'Mircea Marinescu'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.9'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
